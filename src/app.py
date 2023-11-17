b475e6cd7d64a40f0ff838003a6a8c47c6af42f8
mochi-yu <mochi.2225.it@gmail.com>
2023-11-18 01:19:16 +0900

    feat: 必要なエンドポイントを定義

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 import database.db as db
+from models.companyInfo import CreateCompanyRequestParam
 
 app = Flask(__name__)
 CORS(app)
@@ -13,6 +16,38 @@ def getTest():
   else:
     return 404
 
+
+# 企業情報に関するAPI
+# 新しい企業を追加
+@app.route("/company", methods=['POST'])
+def add_company_controller():
+  newCompany = CreateCompanyRequestParam(**request.json)
+
+  
+
+  return 404
+
+# 企業のステータスを変更する
+@app.route("/company/status", methods=['POST'])
+def update_company_status_controller():
+  return 404
+
+# 企業情報取得する
+@app.route("/company", methods=['GET'])
+def get_company_controller():
+  return 404
+
+
+# 予定に関するAPI
+# 新しい予定を追加
+@app.route("/schedule", methods=['POST'])
+def add_schedule_controller():
+  return 404
+
+# 予定を取得する
+@app.route("/schedule", methods=['GET'])
+def get_schedule_controller():
+  return 404
 
 if __name__ == '__main__':
   db.init_db()
