61cd7ceb2ae67fa37d98686d2c15f32ee1694b6e
MiKami12345 <sakaki299@gmail.com>
2023-11-17 15:33:53 +0000

    feat: DB作成

--- src/database/db.py
+++ src/database/db.py
@@ -18,8 +18,62 @@ def _connectDB(isDict = True):
 def init_db():
   try:
     cursor, cnx = _connectDB()
+
+    # Companyテーブルの初期化
+    try:
+        # Companyテーブルがあるかの問い合わせ
+        sql = '''
+            SELECT * FROM Company LIMIT 1;
+        '''
+        cursor.execute(sql)
+        print("Companyテーブルがありました。")
+    except Exception as e:
+        print("Companyテーブルがありませんでした。")
+
+        # Companyテーブルの作成
+        sql = '''
+            CREATE TABLE Company (
+                companyID VARCHAR(255),
+                companyName VARCHAR(255) NOT NULL,
+                memo VARCHAR(255),
+                status int NOT NULL,
+                PRIMARY KEY (CompanyID)
+            );
+        '''
+        cursor.execute(sql)
+        print("Companyテーブルを作成しました。")
+
+
+    # Scheduleテーブルの初期化
+    try:
+        # Scheduleテーブルがあるかの問い合わせ
+        sql = '''
+            SELECT * FROM Schedule LIMIT 1;
+        '''
+        cursor.execute(sql)
+        print("Scheduleテーブルがありました。")
+    except Exception as e:
+        print("Scheduleテーブルがありませんでした。")
+
+        # Scheduleテーブルの作成
+        sql = '''
+            CREATE TABLE Schedule (
+                ID VARCHAR(255) PRIMARY KEY,
+                companyID VARCHAR(255) NOT NULL,
+                userID VARCHAR(255) NOT NULL,
+                title VARCHAR(255) NOT NULL,
+                endDate INT,
+                startDate INT
+            );
+        '''
+        cursor.execute(sql)
+        print("Scheduleテーブルを作成しました。")
   except Exception as e:
     print(f"Error: {e}")
   finally:
     if cnx is not None and cnx.is_connected:
       cnx.close()
+  
+
+
+  
